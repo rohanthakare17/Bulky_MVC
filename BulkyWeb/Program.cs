475350e5c44a9e25ddb29e508360f6462fd5afa7
Rohan <rohanthakare17699@gmail.com>
2023-09-13 15:15:10 +0530

    Edit and Delete

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,14 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configure the Database
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
 
 var app = builder.Build();
 
